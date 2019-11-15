60c37fd35cffbed381889f8ac8c77f0aa37840dc
Matt Neuburg <matt@tidbits.com>
2019-11-16 10:48:44 +1300

    update for iOS 13 scene delegate architecture

--- RegistrationExample/AppDelegate.swift
+++ RegistrationExample/AppDelegate.swift
@@ -4,23 +4,32 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         if let rvc = self.window?.rootViewController {
-            
             let ud = UserDefaults.standard
             if ud.string(forKey: "username") != nil {
+                print("app delegate: user has logged in")
                 let vc = rvc.storyboard!.instantiateViewController(withIdentifier: "userHasLoggedIn")
                 self.window!.rootViewController = vc
             }
         }
         return true
     }
+}
 
-
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let rvc = self.window?.rootViewController {
+            let ud = UserDefaults.standard
+            if ud.string(forKey: "username") != nil {
+                print("scene delegate: user has logged in")
+                let vc = rvc.storyboard!.instantiateViewController(withIdentifier: "userHasLoggedIn")
+                self.window!.rootViewController = vc
+            }
+        }
+    }
 }
 
